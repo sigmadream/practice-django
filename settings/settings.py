9c2082e4090a88fd68c1b451c1c213384ade632e
Sangkon Han <sigmadream@gmail.com>
2022-06-07 04:51:51 +0900

    Category 데이터 추가 및 Admin 개선

--- settings/settings.py
+++ settings/settings.py
@@ -118,6 +118,9 @@ STATICFILES_DIRS = [
     'settings/static'
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
