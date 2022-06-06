8a48c932804a2019ad1f865a71129bd79bab0778
Sangkon Han <sigmadream@gmail.com>
2022-06-07 03:35:31 +0900

    커스텀 사용자 모델 추가

--- settings/views.py
+++ settings/views.py
@@ -1,8 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-# def home(request):
-#     return HttpResponse("Hello, World!")
 
 def home(request):
     return render(request, 'home.html')
