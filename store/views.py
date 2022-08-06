dfbae8fc87868333bf1071b77611ca7d09854f3b
Sangkon Han <sigmadream@gmail.com>
2022-08-06 23:13:56 +0900

    // wip

--- store/views.py
+++ store/views.py
@@ -1,6 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+
+from carts.models import CartItem
 from category.models import Category
 from .models import Product
+
+from carts.views import _cart_id
 
 
 def store(request, category_slug=None):
@@ -26,11 +31,13 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product)
     except Exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        'in_cart': in_cart
     }
 
     return render(request, 'store/product_detail.html', context)
