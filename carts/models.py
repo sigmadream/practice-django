09d3edac60fb2f84a9df5facbc19f7fa14e21b7e
Sangkon Han <sigmadream@gmail.com>
2022-08-15 03:42:09 +0900

    // wip

--- carts/models.py
+++ carts/models.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from django.db import models
 
-from store.models import Product
+from store.models import Product, Variation
 
 
 class Cart(models.Model):
@@ -15,6 +15,7 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variations = models.ManyToManyField(Variation, blank=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
