595ac7fd9d9f8b88e87fddde181c88793d7d9cf6
Sangkon, Han <sigmadream@gmail.com>
2022-10-04 02:01:50 +0900

    //wip: dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -61,7 +61,8 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect('home')
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
@@ -90,3 +91,8 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, "Invalid activation link")
         return HttpResponse('register')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
