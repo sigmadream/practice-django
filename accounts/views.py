6405b8c5d89f88bc452f42a358efdb2a0d495eb7
Sangkon, Han <sigmadream@gmail.com>
2022-11-13 03:29:32 +0900

    //wip: flake8

--- accounts/views.py
+++ accounts/views.py
@@ -14,85 +14,91 @@ from .models import Account
 
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            phone_number = form.cleaned_data['phone_number']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            phone_number = form.cleaned_data["phone_number"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
             username = email.split("@")[0]
             print(first_name, last_name, phone_number, email, password, username)
-            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email,
-                                               username=username, password=password)
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password,
+            )
             user.phone_number = phone_number
             user.save()
 
             current_site = get_current_site(request)
-            mail_subject = 'Please activate your account'
-            message = render_to_string('accounts/account_verification_email.html', {
-                'user': user,
-                'domain': current_site,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': default_token_generator.make_token(user),
-            })
+            mail_subject = "Please activate your account"
+            message = render_to_string(
+                "accounts/account_verification_email.html",
+                {
+                    "user": user,
+                    "domain": current_site,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "token": default_token_generator.make_token(user),
+                },
+            )
             to_email = email
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send()
             # messages.success(request,
             #                  "Thank you for registering with us. We have send you a verification email to your email address. Please verify it.")
-            return redirect('/accounts/login/?command=verification&email=', email)
+            return redirect("/accounts/login/?command=verification&email=", email)
     else:
         form = RegistrationForm()
 
-    context = {
-        'form': form
-    }
-    return render(request, 'accounts/register.html', context)
+    context = {"form": form}
+    return render(request, "accounts/register.html", context)
 
 
 def login(request):
-    if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
 
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
             auth.login(request, user)
-            messages.success(request, 'You are now logged in.')
-            return redirect('dashboard')
+            messages.success(request, "You are now logged in.")
+            return redirect("dashboard")
         else:
-            messages.error(request, 'Invalid login credentials')
-            return redirect('login')
-    return render(request, 'accounts/login.html')
+            messages.error(request, "Invalid login credentials")
+            return redirect("login")
+    return render(request, "accounts/login.html")
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def logout(request):
     auth.logout(request)
-    messages.success(request, 'You are logged out.')
-    return redirect('logout')
+    messages.success(request, "You are logged out.")
+    return redirect("logout")
 
 
 def activate(request, uidb64, token):
     try:
         uid = urlsafe_base64_decode(uidb64).decode()
         user = Account._default_manager.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+    except (TypeError, ValueError, OverflowError, Account.DoesNotExist):
         user = None
 
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
         messages.success(request, "Congratulations! Your account is activated.")
-        return redirect('login')
+        return redirect("login")
     else:
         messages.error(request, "Invalid activation link")
-        return HttpResponse('register')
+        return HttpResponse("register")
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    return render(request, "accounts/dashboard.html")
