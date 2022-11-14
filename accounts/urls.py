fc5c1a0d95bcf647306624bdaddb89b7537b7610
Sangkon, Han <sigmadream@gmail.com>
2022-11-15 02:14:46 +0900

    사용자 비밀번호 관련 추가 (#4)
    사용자 비밀번호 설정

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('activate/<uidb64>/<token>/', views.activate, name='activate')
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("", views.dashboard, name="dashboard"),
+    path("activate/<uidb64>/<token>/", views.activate, name="activate"),
+    path("forgot-password/", views.forgot_password, name="forgot-password"),
+    path(
+        "reset-password-validate/<uidb64>/<token>/",
+        views.reset_password_validate,
+        name="reset-password-validate",
+    ),
+    path("reset-password/", views.reset_password, name="reset-password"),
 ]
